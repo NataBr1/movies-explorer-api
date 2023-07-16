25e567ecb768e9dc9d7a26fce6e02be118a90e3d
Natalya Bryzgalova <Natasha.Brizgalova@yandex.rugit config --global user.email Natasha.Brizgalova@yandex.ru>
2023-07-16 13:30:38 +0300

    Пробуем загрузить код бэкенда на сервер

---
+++ controllers/movies.js
@@ -0,0 +1,46 @@
+const Movie = require('../models/movie');
+const NotFoundError = require('../errors/not-found-err');
+const BadRequestError = require('../errors/bad_request_err');
+const AccessDeniedError = require('../errors/access_denied_err');
+
+// возвращает все сохранённые текущим  пользователем фильмы
+const getMovies = (req, res, next) => {
+  Movie.find({})
+    .then((movie) => res.status(200).send(movie))
+    .catch(next);
+};
+
+// создаёт фильм с переданными в теле значениями
+const createMovies = (req, res, next) => {
+  Movie.create({ owner: req.user._id, ...req.body })
+    .then((movie) => res.status(201).send(movie))
+    .catch((err) => {
+      if (err.message.includes('validation failed')) {
+        next(new BadRequestError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+// удаляет сохранённый фильм по id
+const deleteMovies = (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .orFail(() => {
+      throw new NotFoundError('Передан несуществующий _id фильма');
+    })
+    .then((movie) => {
+      if (String(movie.owner) === req.user._id) {
+        Movie.findByIdAndRemove(req.params.movieId).then(() => res.status(200).send(movie));
+      } else {
+        throw new AccessDeniedError('Нет прав на удаление этого фильма');
+      }
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getMovies,
+  createMovies,
+  deleteMovies,
+};
---
+++ models/movie.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: [true, 'Обязательное поле'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Обязательное поле'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Обязательное поле'],
+  },
+  year: {
+    type: String,
+    required: [true, 'Обязательное поле'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Обязательное поле'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Обязательное поле'],
+  },
+  trailerLink: {
+    type: String,
+    required: [true, 'Обязательное поле'],
+  },
+  thumbnail: {
+    type: String,
+    required: [true, 'Обязательное поле'],
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'Обязательное поле'],
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'Обязательное поле'],
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
