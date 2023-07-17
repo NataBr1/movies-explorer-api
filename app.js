b05347743f42c216270df54c46a4a08c33909a46
Natalya Bryzgalova <Natasha.Brizgalova@yandex.rugit config --global user.email Natasha.Brizgalova@yandex.ru>
2023-07-17 22:28:06 +0300

    Добавлен cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error');
@@ -13,6 +14,16 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
+
+app.use(cors({
+  credentials: true,
+  origin: [
+    'https://movies.nb.nomoredomains.work',
+    'https://movies.nomoredomains.xyz',
+    'http://localhost:3000',
+    'http://localhost:3001',
+  ],
+}));
 
 app.use(router);
 
